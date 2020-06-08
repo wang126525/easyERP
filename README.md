2467bdebd26b5cde2e6f3ad1017a1afefe2bd5ae
王涛0525 <2443897051@qq.com>
2020-06-08 10:06:38 +0800

    清空

--- README.md
+++
@@ -1,6 +0,0 @@
-# easyERP
-
-##### FrontEnd--Powered by Vue.js
-##### BackEnd--Powered by Node.js
-##### DataBase--mongoDB
-##### Special thanks to axios,bcrypt,jsonwebtoken,viser-vue,xlsx and so on and so forth.
--- src/api/supplier.js
+++
@@ -1,9 +0,0 @@
-import request from "@/axios";
-
-// 添加供应商
-export const add = params =>
-  request({ url: "/api/supplier/add", method: "post", data: params });
-
-// 添加供应商
-export const getSupplierList = params =>
-  request({ url: "/api/supplier/pagination", method: "get", params: params });
